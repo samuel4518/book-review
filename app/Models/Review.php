ee42169378f185b6563248673baba9e587f7dcd9
Hsin-En Lee <samuel4518@gmail.com>
2025-05-09 00:08:08 +0800

    update factory

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,9 @@ class Review extends Model
 {
     //
     use HasFactory;
+
+    protected $fillable = ['review', 'rating'];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
