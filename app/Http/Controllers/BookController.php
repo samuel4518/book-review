a08b55563115342d169c30cb7d701b3e886f339b
Hsin-En Lee <samuel4518@gmail.com>
2025-06-05 00:13:21 +0800

    update: popular/highest rated logic

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,15 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
+        $books = Book::when(
+            $title,
+            fn($query, $title) => $query->title($title)
+        );
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $books = $books->get();
+
+        return view('books.index', ['books' => $books]);
     }
 
     /**
